976b109ed349c975cee1f168908a38531782c71e
Xavier M.W <xavifmw@gmail.com>
2022-12-07 08:10:22 -0700

    Created Builder Coroutine

--- 2022/Day7/main.py
+++ 2022/Day7/main.py
@@ -1,12 +1,22 @@
 
-files = {}
 INPUT = "input.txt"
+
+
+### Build File System ###
+
+file_tree = {}
 
 
 def build_file_tree(path_in):
 
-    with open(path_in, "r") as commands:
-        lines = format_commands(commands.readlines())
+    with open(path_in, "r") as input_file:
+        commands = format_commands(input_file.readlines())
+
+        builder = file_tree_builder()
+        next(builder)  # Priming coroutine
+
+        for command in commands:
+            builder.send(command)
 
 
 def format_commands(lines):
@@ -18,17 +28,62 @@ def format_commands(lines):
     return output
 
 
-# Replace with Generator Coroutine
-def builder_factory():
+def file_tree_builder():
     current_file = None
+    while True:
+        command = yield
+        first, second = parse_command(command)
 
-    def builder(command):
-        unzipped = command.split()
-        if unzipped[0] == "cd":
-            key = unzipped[1]
+        if first == "cd":
+            key = f"directory_{second}"
+            file_tree[key] = []
             current_file = key
 
+        elif first.isnumeric():
+            file_tree[second] = int(first)
+            file_tree[current_file].append(second)
+
+        else:
+            key = f"directory_{first}"
+            file_tree[current_file].append(key)
+
+
+def parse_command(command):
+    try:
+        first, second = command.split()
+    except ValueError:
+        first, second = command, ""
+    return first, second
+
+
+### Operations on File System ###
+
+def get_size(key):
+    size = 0
+    mapped = file_tree[key]
+
+    if isinstance(mapped, int):
+        size += mapped
+
+    else:
+        for item in mapped:
+            size += get_size(item)
+
+    return size
+
+
+def get_sizes_up_to_threshold(threshold):
+    total = 0
+    for key in file_tree.keys():
+        size = get_size(key)
+        if size <= threshold:
+            total += size
+    return total
 
 
 if __name__ == "__main__":
     build_file_tree(INPUT)
+    for key in file_tree.keys():
+        print(
+            key, get_size(key)
+        )
