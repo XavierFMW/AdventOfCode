fcfe994ffe7a886b264f4bce95130974c39f1e15
Xavier M.W <xavifmw@gmail.com>
2022-12-09 06:28:58 -0700

    Poorly attempted part 2

--- 2022/Day9/main.py
+++ 2022/Day9/main.py
@@ -20,63 +20,59 @@ def build_moves(path_in):
     return moves
 
 
-def move_ends(positions, move):
-    occupied = set()
-    (hx, hy), (tx, ty) = positions
-    key, distance = move
-
-    dx, dy = MOVES[key]
-    for _ in range(distance):
-        # view_board(
-        #    ((hx, hy), (tx, ty))
-        # )
-        ohx, ohy = hx, hy
-        hx += dx
-        hy += dy
-
-        if abs(hx - tx) > abs(hy - ty) and abs(hx - tx) > 1 or abs(hy - ty) > abs(hx - tx) and abs(hy - ty) > 1:
-            tx, ty = ohx, ohy
-        elif abs(hx - tx) > 1 or abs(hy - ty) > 1:
-            tx += dx
-            ty += dy
-
-        occupied.add((tx, ty))
-
-    return (hx, hy), (tx, ty), occupied
-
-
-def get_all_tail_positions(moves):
-    hx, hy = 0, 0
-    tx, ty = 0, 0
-    tail_positions = {(tx, ty)}
+def move_segments(moves, segments=2):
+    tail_occupied = set()
+    rope = tuple([0, 0] for _ in range(segments))
+    head = rope[0]
 
     for move in moves:
-        (hx, hy), (tx, ty), occupied = move_ends(
-            ((hx, hy), (tx, ty)), move
-        )
-        tail_positions.update(occupied)
-    return tail_positions
+        key, distance = move
+        dx, dy = MOVES[key]
+
+        for _ in range(distance):
+            view_board(rope)
+            ohx, ohy = head
+            head[0] += dx
+            head[1] += dy
+            for index in range(1, segments):
+                leader = rope[index - 1]
+                follower = rope[index]
+                hx, hy = leader
+                tx, ty = follower
+                if abs(hx - tx) > abs(hy - ty) and abs(hx - tx) > 1:
+                    follower[0] = hx
+                    follower[1] = hy + (ty - hy)
+                elif abs(hy - ty) > abs(hx - tx) and abs(hy - ty) > 1:
+                    follower[0] = hx + (tx - hx)
+                    follower[1] = hy
+                elif abs(hx - tx) > 1 or abs(hy - ty) > 1:
+                    follower[0] += dx
+                    follower[1] += dy
+                ohx, ohy = tx, ty
+
+                if index == segments - 1:
+                    tail_occupied.add(tuple(rope[index]))
+
+    return tail_occupied
 
 
-def view_board(positions, width=6, height=5):
-    (hx, hy), (tx, ty) = positions
-    printed = ""
+def view_board(rope, width=6, height=5):
+    printed = []
 
     print("=" * width)
     for y in range(height):
+        row = ""
         for x in range(width):
-            if x == hx and y == hy:
-                printed += "H"
-            elif x == tx and y == ty:
-                printed += "T"
+            if [x, y] in rope:
+                row += str(rope.index([x, y]))
             else:
-                printed += "."
-        printed += "\n"
-    print(printed, "\n")
+                row += "."
+        printed.append(row)
+    for r in printed[::-1]:
+        print(r)
+    print("\n")
 
 
 if __name__ == "__main__":
     m = build_moves(INPUT)
-    print(
-        len(sorted(get_all_tail_positions(m)))
-    )
+    print(len(move_segments(m, 10)))
